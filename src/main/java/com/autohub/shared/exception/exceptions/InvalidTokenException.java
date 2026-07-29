191ec0cd0e4f9b835afe5583ea7e226a1fcaddfd
guiPinheiro. <guivalen00@gmail.com>
2026-07-29 16:44:31 -0300

    Create InvalidTokenException class

---
+++ src/main/java/com/autohub/shared/exception/exceptions/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.autohub.shared.exception.exceptions;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
