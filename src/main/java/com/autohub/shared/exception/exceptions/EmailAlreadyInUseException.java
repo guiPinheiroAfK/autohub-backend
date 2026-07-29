898de52eb671e42598a826a799950ffa4f1220b4
guiPinheiro. <guivalen00@gmail.com>
2026-07-29 16:44:06 -0300

    Create EmailAlreadyInUseException.java

---
+++ src/main/java/com/autohub/shared/exception/exceptions/EmailAlreadyInUseException.java
@@ -0,0 +1,7 @@
+package com.autohub.shared.exception.exceptions;
+
+public class EmailAlreadyInUseException extends RuntimeException {
+    public EmailAlreadyInUseException(String email) {
+        super("Email already in use: " + email);
+    }
+}
